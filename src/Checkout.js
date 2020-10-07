a880b425c154060aaf5d7e31960b7699a29bde0a
kenchen <a330120231@gmail.com>
2020-10-06 22:44:33 -0700

    deploy test

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,12 +5,13 @@ import { useStateValue } from './StateProvider'
 import CheckoutProduct from './CheckoutProduct';
 
 function Checkout() {
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket,user}, dispatch] = useStateValue();
     return (
         <div className="checkout">
              <div className="checkout_left">
                 <img className="checkout_ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423592668_.jpg" alt=""/>
                 <div>
+    <h3>Hello, {user?.email}</h3>
                     <h2 className="checkout_title"> Your shopping basket</h2>
                     {basket.map(item=>(
                         <CheckoutProduct 
--- src/Login.js
+++ src/Login.js
@@ -1,86 +1,69 @@
-import { auth } from "./firebase";
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import './Login.css'
 import { Link, useHistory } from "react-router-dom";
-import "./Login.css";
-
+import { auth } from "./firebase";
 
 function Login() {
-  const history = useHistory();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  const signIn = (e) => {
-    e.preventDefault();
+    const signIn = e => {
+        e.preventDefault();
 
-    auth
-      .signInWithEmailAndPassword(email, password)
-      .auth((auth) => {
-        console.log(auth);
-        history.push("/");
-      })
-      .catch((error) => alert(error.message));
-  };
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
+    }
 
-  const register = (e) => {
-    e.preventDefault();
+    const register = e => {
+        e.preventDefault();
 
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then((auth) => {
-        //   it successfully created a new user with email and password
-        console.log(auth);
-        if (auth) {
-          history.push("/");
-        }
-      })
-      .catch((error) => alert(error.message));
-  };
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // it successfully created a new user with email and password
+                if (auth) {
+                    history.push('/')
+                }
+            })
+            .catch(error => alert(error.message))
+    }
 
-  return (
-    <div className="login">
-      <Link to="/">
-        <img
-          className="login_logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
-          alt=""
-        />
-      </Link>
+    return (
+        <div className='login'>
+            <Link to='/'>
+                <img
+                    className="login_logo"
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' 
+                />
+            </Link>
 
-      <div className="login_container">
-        <h1>Sign-in</h1>
+            <div className='login_container'>
+                <h1>Sign-in</h1>
 
-        <form>
-          <h5>E-mail</h5>
-          <input
-            value={email}
-            type="text"
-            onChange={(e) => setEmail(e.target.value)}
-          />
+                <form>
+                    <h5>E-mail</h5>
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
 
-          <h5>Password</h5>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+                    <h5>Password</h5>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
 
-          <button type="submit" onClick={signIn} className="login_signInButton">
-            Sign In
-          </button>
-        </form>
+                    <button type='submit' onClick={signIn} className='login_signInButton'>Sign In</button>
+                </form>
 
-        <p>
-          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
-          Sale. Please see our Privacy Notice, our Cookies Notice and our
-          Interest-Based Ads Notice.
-        </p>
+                <p>
+                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
+                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
 
-        <button onClick={register} className="logi_registerButton">
-          Create your Amazon Account
-        </button>
-      </div>
-    </div>
-  );
+                <button onClick={register} className='login_registerButton'>Create your Amazon Account</button>
+            </div>
+        </div>
+    )
 }
 
-export default Login;
+export default Login
